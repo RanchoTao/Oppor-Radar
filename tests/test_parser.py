84aaf5913411abee120a31201b805ab289859193
RanchoTao <72544580+RanchoTao@users.noreply.github.com>
2026-08-18 20:36:46 +0800

    test: cover generalized source watch filtering

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,7 +10,7 @@ def keywords():
     }
 
 
-def test_source_tags_alone_do_not_make_irrelevant_link_a_candidate():
+def test_source_tags_alone_do_not_make_irrelevant_navigation_candidate():
     html = """
     <html><body>
       <div><a href="/about">联系我们</a></div>
@@ -27,10 +27,11 @@ def test_source_tags_alone_do_not_make_irrelevant_link_a_candidate():
     assert items == []
 
 
-def test_relevant_link_text_is_kept():
+def test_relevant_link_text_is_kept_without_application_only_filter():
     html = """
     <html><body>
       <div>面向本科生开放 <a href="/summer">AI 科研训练报名</a></div>
+      <div><a href="/paper">新的机器学习研究结果发布</a></div>
     </body></html>
     """
     source = {
@@ -41,6 +42,25 @@ def test_relevant_link_text_is_kept():
 
     items = parse_opportunities(html, source, keywords())
 
-    assert len(items) == 1
-    assert items[0].title == "AI 科研训练报名"
+    assert len(items) == 2
     assert items[0].url == "https://example.edu/summer"
+    assert items[1].url == "https://example.edu/paper"
+
+
+def test_watch_keywords_support_finance_or_any_other_group():
+    html = """
+    <html><body>
+      <div><a href="/macro">央行发布最新宏观政策报告</a></div>
+      <div><a href="/sports">球队公布新赛季名单</a></div>
+    </body></html>
+    """
+    source = {
+        "name": "Example News",
+        "url": "https://example.com/",
+        "group": "金融",
+        "watch": ["宏观", "央行"],
+    }
+
+    items = parse_opportunities(html, source, keywords())
+
+    assert [item.title for item in items] == ["央行发布最新宏观政策报告"]
